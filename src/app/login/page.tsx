3c608c4347a3d6900ee8db0fbeef550ca7f90ff3
h4manshu <h4manshu@gmail.com>
2024-03-12 14:27:49 +0530

    env display

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,9 +4,12 @@ import {RegisterLink, LoginLink} from "@kinde-oss/kinde-auth-nextjs/components";
 const Login = () => {
    
     console.log(process.env.ENV_CHECK)
+   
     return (  
     <main>
         {process.env.ENV_CHECK}
+        {process.env.KINDE_CLIENT_SECRET}
+        {process.env.KINDE_CLIENT_ID}
     <h1> Login Page </h1>
 
         <LoginLink postLoginRedirectURL="/">Sign in</LoginLink>
