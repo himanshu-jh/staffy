5af7fb7f3bdee07bd6a66b9b9c8dbcca95ba466e
h4manshu <h4manshu@gmail.com>
2024-03-16 08:26:44 +0530

    hello

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,21 +1,29 @@
-import {RegisterLink, LoginLink} from "@kinde-oss/kinde-auth-nextjs/components";
-
+import React from 'react';
+import { RegisterLink, LoginLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import { Button } from "@/components/ui/button";
 
 const Login = () => {
-   
-    console.log(process.env.ENV_CHECK)
-   
     return (  
-    <main>
-        {process.env.ENV_CHECK}
-    <h1> Login Page </h1>
-
-        <LoginLink postLoginRedirectURL="/">Sign in</LoginLink>
-        <RegisterLink postLoginRedirectURL="/welcome">Sign up</RegisterLink>
-    </main>
-   
+        <main className="flex justify-center items-center h-screen">
+            <div className="container">
+                <div className="row justify-content-center align-items-center">
+                    <div className="col-md-6 text-center">
+                        <img src="/zamy_logo.png" alt="Logo" className="mb-4 mx-auto" style={{ maxWidth: '200px' }} />
+                        <h1 className="text-3xl font-bold mb-6">Welcome</h1>
+                        <p className="text-lg font-semibold mb-6">Please sign in or register to continue</p>
+                        <div className="mb-4">
+                            <LoginLink postLoginRedirectURL="/">
+                                <Button className="mb-3" style={{ maxWidth: '300px', width: '100%', display: 'block', margin: '0 auto 10px' }}>Sign In</Button>
+                            </LoginLink>
+                            <RegisterLink postLoginRedirectURL="/welcome">
+                                <Button style={{ maxWidth: '300px', width: '100%', display: 'block', margin: '0 auto' }}>Register</Button>
+                            </RegisterLink>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
     );
 }
 
 export default Login;
-
---
+++ src/components/api.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { Option } from "@/components/searchBar";
+
+const apiUrl = 'http://localhost:5100/';
+const apiEmpNameRoute = 'api/employees/names';
+
+export const useEmployeeNames = () => {
+    const [employeeNames, setEmployeeNames] = useState<Option[]>([]);
+
+    const fetchEmployeeNames = async () => {
+        try {
+            const response = await fetch(apiUrl + apiEmpNameRoute);
+            if (response.ok) {
+                const data = await response.json();
+                const options = data.map((employee: { name: any; }) => ({
+                    label: employee.name,
+                    value: employee.name,
+                }));
+                setEmployeeNames(options);
+            } else {
+                console.error('Failed to fetch employee names');
+            }
+        } catch (error) {
+            console.error('Error during API call:', error);
+        }
+    };
+
+    // Call the fetchEmployeeNames function when this hook is used
+    useState(() => {
+        fetchEmployeeNames();
+    }, []);
+
+    return employeeNames;
+};
